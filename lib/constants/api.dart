07039be7106c3dd4e3d87325ae76617a64b077a5
Henry <pienry3g@gmail.com>
2019-11-05 09:13:01 +0100

    remote api utilization

--- lib/constants/api.dart
+++ lib/constants/api.dart
@@ -1,4 +1,5 @@
 class Api {
-  static String baseUrl = 'http://localhost:4225/api/v1/auth/';
+  static String baseUrl = 'https://advertise-it.herokuapp.com/';
+  // static String baseUrl = 'http://localhost:4225/api/v1/auth/';
   static String loginUrl = '$baseUrl/login';
 }
